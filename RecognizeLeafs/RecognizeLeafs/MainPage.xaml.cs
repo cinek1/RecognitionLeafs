c5bcdcbfabc96c765b6fb56ae375ca141a85a689
unknown <mgalas1@op.pl>
2021-01-11 17:13:05 +0100

    first commit

---
+++ RecognizeLeafs/RecognizeLeafs/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RecognizeLeafs
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+        private State state; 
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            this.BindingContext = viewModel;
+            state = State.Choose;
+            viewModel.AnalyzeInfo = "Do you want recognize Tree?";
+        }
+
+
+        private async void Button_Choose_Clicked(object sender, EventArgs e)
+        {
+            if (state == State.Analyze)
+            {
+                await Analyze(); 
+            }
+            else
+            {
+                await ChooseFile(); 
+            }
+        }
+
+        private async Task Analyze()
+        {
+            viewModel.AnalyzeInfo = "Tree: OAK \n";
+            viewModel.WikipediaLink = "https://en.wikipedia.org/wiki/Oak";
+            Button_Choose.IsEnabled = false; 
+        }
+
+        private void Exit()
+        {
+            ChangeButtonsToChoose();
+            viewModel.ImageSource = ""; 
+        }
+
+        private async Task ChooseFile()
+        {
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if(file != null)
+            {
+                this.viewModel.ImageSource = file.Path;
+                ChangeButtonsToAnalyze(); 
+            }
+        }
+
+        private void ChangeButtonsToAnalyze()
+        {
+            viewModel.Choose = "Analyze";
+            viewModel.TakePhoto = "Exit";
+            state = State.Analyze;
+            viewModel.AnalyzeInfo = "";
+
+        }
+
+
+        private void ChangeButtonsToChoose()
+        {
+            viewModel.Choose = "Choose photo";
+            viewModel.TakePhoto = "Take photo";
+            viewModel.AnalyzeInfo = "Do you want recognize Tree?";
+            viewModel.WikipediaLink = "";
+            state = State.Choose;
+            Button_Choose.IsEnabled = true;
+
+        }
+        private async void Button_Take_Clicked(object sender, EventArgs e)
+        {
+            if (state == State.Analyze)
+            {
+                 Exit();
+            }
+            else
+            {
+                await ChooseFile();
+            }
+        }
+    }
+}
---
+++ RecognizeLeafs/RecognizeLeafs/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace RecognizeLeafs
+{
+    class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string takePhoto; 
+
+        public string TakePhoto { get { return takePhoto; } set { takePhoto = value; OnPropertyChanged("TakePhoto"); } }
+
+        private string imageSource; 
+
+        public string ImageSource { get { return imageSource; } set { imageSource = value; OnPropertyChanged("ImageSource"); } }
+
+        private string choose;
+
+        public string Choose { get { return choose; } set { choose = value; OnPropertyChanged("Choose"); } }
+
+        private string analyzeInfo;
+
+        public string AnalyzeInfo { get { return analyzeInfo; } set { analyzeInfo = value; OnPropertyChanged("AnalyzeInfo"); } }
+
+        public string wikipediaLink;
+
+        public string WikipediaLink { get { return wikipediaLink; } set { wikipediaLink = value; OnPropertyChanged("WikipediaLink"); } }
+
+        public MainPageViewModel()
+        {
+            TakePhoto = "Take photo";
+            ImageSource = "";
+            AnalyzeInfo = "Do you want recognize tree?";
+            Choose = "Chose photo";
+        }
+    }
+}
